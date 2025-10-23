26352aaa506400cd2657adb5c911b1296d1e0c53
vayu <Vayuswaraj2212521@gmail.com>
2025-10-23 07:17:55 +0530

    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const server = express();
+const path = require('path');
+require('dotenv').config();
+
+server.use(express.static(path.join(__dirname,"./")));
+
+const links = [
+  "/",
+  "/dashboard",
+  "/entries",
+  "/gallery",
+  "/contacts",
+  "/passwords",
+  "/login",
+  "/home",
+  "/signup",
+];
+
+function renderPage(res, link) {
+  res.sendFile(path.join(__dirname, `./${link.toLowerCase()}.html`));
+}
+
+links.forEach((link) => {
+  server.get(`/${link.toLowerCase()}`, (req, res) => {
+    renderPage(res, "index");
+  });
+});
+
+module.exports = server;
